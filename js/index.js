0752b5a07de6d42ae4112042c16ec6fa91a5ae24
Jim-KingKong <Jim-KingKong@example.com>
2018-11-22 13:23:02 +0800

    更新了换肤特效

--- js/index.js
+++ js/index.js
@@ -3,18 +3,45 @@ $(function () {
     var changeSkin = $('#bgcUl');
     var origin = $('.origin');
     var obody = $('body');
+    var currentTop = 0;
+    var currentLeft = 0;
+    var arrM =[];
     origin.click(function (e) {
         obody.css({
-            'backgroundColor':'#fff'
+            'backgroundColor':'#f1f1f4'
+        });
+        e.stopPropagation();
+
+        $.each(changeSkin.find('li'),function (i) {
+             currentTop = window.getComputedStyle(this).top;
+             currentLeft = window.getComputedStyle(this).left;
+             if (arrM.length<=3) arrM.push({top:currentTop,left:currentLeft});
+             var inTop = arrM[i].top;
+             var inLeft = arrM[i].left;
+
+            if (!$(this).hasClass('.disn')){
+            $(this).animate({
+                top:0,
+                left:0,
+                opacity:0
+            }).addClass('.disn');
+            }else{
+                console.log(currentTop);
+                $(this).animate({
+                    top:inTop,
+                    left:inLeft,
+                    opacity:1
+                }).removeClass('.disn')
+            }
+
+    });
+    changeSkin.click(function (e) {
+        changeSkin.find('#bgcUl').stop().fadeIn(1000);
+
         });
         e.stopPropagation();
     });
-    changeSkin.click(function (e) {
-        changeSkin.find('#bgcUl').fadeIn(1000);
-        e.stopPropagation();
-    });
     changeSkin.find('li').each(function () {
-        console.log(1);
         $(this).click(function (e) {
 
             var ownColor = this.style.backgroundColor;
@@ -46,10 +73,10 @@ $(function () {
         $(".sidebar").slideToggle(500)
     });
     $(".mask").on("click",function () {
-        $(".mask-D").css("display","block")
+        $(".mask-D").fadeIn(1000)
     });
     $(".mask-D").on("click",function () {
-        $(".mask-D").css("display","none")
+        $(".mask-D").fadeOut(1000)
     });
     $(".bottom-btn-img").on("click",function (e) {
         $(".bottom-btn").slideToggle(500);
