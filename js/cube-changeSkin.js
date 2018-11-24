73d9bdb834e2ff3c7cdc64e43126a62a53b7f373
Jim-KingKong <Jim-KingKong@example.com>
2018-11-24 17:15:03 +0800

    最终展示版本

--- js/cube-changeSkin.js
+++ js/cube-changeSkin.js
@@ -3,8 +3,8 @@ $(function (){
     var cubeBoxSkin = $('.cubebox-changeSkin');
     var selfY = $('body').height();
     var clientY = $(window).height();
-    var testLeft = 0
-    var Skinflag= false;
+    // var testLeft = 0
+    // var Skinflag= false;
     $(window).on('scroll',function () {
         //箭头消失
         $('.arrow img').fadeOut(1000);
@@ -37,8 +37,8 @@ $(function (){
 
 
             $(this).click(function (e) {
-                testLeft  = cubeBoxSkin.offset().left;
-                if (Skinflag) return
+                // testLeft  = cubeBoxSkin.offset().left;
+                // if (Skinflag) return
                 e.stopPropagation();
                 var index = i;
                 var picParent = $($('.changeskin-box .m-pic').get(i));
@@ -69,37 +69,37 @@ $(function (){
         e.stopPropagation();
     });
     //拖拽事件
-    var flag = false;
-    cubeBoxSkin.mousedown(function (event) {
-        flag = false;
-        var e = event || window.event;
-        var clientX = e.clientX;
-        var clientY = e.clientY;
-        var currentTop = cubeBoxSkin.offset().top;
-        var currentLeft = cubeBoxSkin.offset().left;
-        var scrollY = $(document).scrollTop();
-        var distanceY = clientY - currentTop;
-        var distanceX = clientX - currentLeft;
-
-        console.log(scrollY);
-        $(document).mousemove(function (event) {
-            if (flag == true) return null;
-            var e = event || window.event;
-            var currentX = e.clientX;
-            var currentY = e.clientY;
-            cubeBoxSkin.css({
-                top:currentY-distanceY-scrollY,
-                left:currentX-distanceX
-            });
-            return false
-        });
-        return false
-    });
-    $(document).mouseup(function (e) {
-        var currentTestLeft = cubeBoxSkin.offset().left;
-        currentTestLeft!==testLeft? Skinflag = true : Skinflag = false;
-        flag = true;
-
-        e.stopPropagation()
-    })
+    // var flag = false;
+    // cubeBoxSkin.mousedown(function (event) {
+    //     flag = false;
+    //     var e = event || window.event;
+    //     var clientX = e.clientX;
+    //     var clientY = e.clientY;
+    //     var currentTop = cubeBoxSkin.offset().top;
+    //     var currentLeft = cubeBoxSkin.offset().left;
+    //     var scrollY = $(document).scrollTop();
+    //     var distanceY = clientY - currentTop;
+    //     var distanceX = clientX - currentLeft;
+    //
+    //     console.log(scrollY);
+    //     $(document).mousemove(function (event) {
+    //         if (flag == true) return null;
+    //         var e = event || window.event;
+    //         var currentX = e.clientX;
+    //         var currentY = e.clientY;
+    //         cubeBoxSkin.css({
+    //             top:currentY-distanceY-scrollY,
+    //             left:currentX-distanceX
+    //         });
+    //         return false
+    //     });
+    //     return false
+    // });
+    // $(document).mouseup(function (e) {
+    //     var currentTestLeft = cubeBoxSkin.offset().left;
+    //     currentTestLeft!==testLeft? Skinflag = true : Skinflag = false;
+    //     flag = true;
+    //
+    //     e.stopPropagation()
+    // })
 });
